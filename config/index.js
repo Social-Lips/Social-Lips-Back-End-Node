d31fdf40be280b18063454dccbe460ee7c0bc742
deshan68 <arundeshan>
2023-08-05 20:37:02 +0530

    remove env values

--- config/index.js
+++ config/index.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
 const { S3Client } = require("@aws-sdk/client-s3");
 
-const REGION = "ap-south-1";
-const ACCESS_KEY = "AKIATVMCFFMCIUHMGIXF";
-const ACCESS_SECRET = "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";
-
 const s3 = new S3Client({
   region: process.env.REGION,
   credentials: {
